dece670650b08df8a044b019e1cd4e07f6b12120
Chanceedson <chancejackson341@gmail.com>
2026-02-15 20:36:12 +0300

    modified:   devread_core/settings.py

--- devread_core/settings.py
+++ devread_core/settings.py
@@ -133,13 +133,3 @@ BASE_DIR / 'devread_core/static/',
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # For development
-# For production, change to:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'chancejack143@gmail.com'
